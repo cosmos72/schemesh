e5d6417b73eb1117c8bad4d1edb73024ea13d5fd
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2025-02-12 22:27:44 +0100

    fix script utils/find_chez_scheme_dir.sh broken by previous commit

--- utils/find_chez_scheme_dir.sh
+++ utils/find_chez_scheme_dir.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-chezscheme --verbose < /dev/null 2>&1 | grep 'scheme.boot...opened' | sed -e 's,^trying ,,g' -e 's,/scheme.boot...opened,,g' || \
+chezscheme --verbose < /dev/null 2>&1 | grep 'chezscheme.boot...opened' | sed -e 's,^trying ,,g' -e 's,/chezscheme.boot...opened,,g' || \
     scheme --verbose < /dev/null 2>&1 | grep 'scheme.boot...opened' | sed -e 's,^trying ,,g' -e 's,/scheme.boot...opened,,g'
