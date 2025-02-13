c88d6af9cccc04b2efa07be452000566e21efa37
elb <elb@disroot.org>
2025-02-13 17:38:15 +0100

    fix utils/find_chez_scheme_dir.sh for alpine linux

--- utils/find_chez_scheme_dir.sh
+++ utils/find_chez_scheme_dir.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-( chezscheme --verbose < /dev/null 2>&1 || scheme --verbose < /dev/null 2>&1 ) \
-  | grep 'scheme.boot...opened' \
-  | sed -e 's,^trying ,,g' -e 's,/chezscheme.boot...opened,,g' -e 's,/scheme.boot...opened,,g'
+( chezscheme --verbose < /dev/null 2>&1 || scheme --verbose < /dev/null 2>&1 || chez --verbose < /dev/null 2>&1 ) \
+  | grep -E '(chez|scheme)\.boot\.\.\.opened' \
+  | sed -e 's,^trying ,,g' -e 's,/chezscheme.boot...opened,,g' -e 's,/scheme.boot...opened,,g' -e 's,/chez.boot...opened,,g'
