bec5a0ea089034248028d22fa122fef27f85830d
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2025-02-13 22:15:24 +0100

    reorder possible names of Chez Scheme executables in utils/find_chez_scheme_dir.sh putting `scheme` as last, because it's the most generic and may point to other Scheme implementations

--- utils/find_chez_scheme_dir.sh
+++ utils/find_chez_scheme_dir.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-( chezscheme --verbose < /dev/null 2>&1 || scheme --verbose < /dev/null 2>&1 || chez --verbose < /dev/null 2>&1 ) \
+( chezscheme --verbose || chez --verbose || scheme --verbose ) </dev/null 2>&1 \
   | grep -E '(chez|scheme)\.boot\.\.\.opened' \
-  | sed -e 's,^trying ,,g' -e 's,/chezscheme.boot...opened,,g' -e 's,/scheme.boot...opened,,g' -e 's,/chez.boot...opened,,g'
+  | sed -e 's,^trying ,,g' -e 's,/chezscheme.boot...opened,,g' -e 's,/chez.boot...opened,,g' -e 's,/scheme.boot...opened,,g'
